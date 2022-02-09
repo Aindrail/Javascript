ebeb5eef4cd5fe4a64a37943e513b25eba760399
Aindrail <aindrailsantra@gmail.com>
2022-02-10 01:33:59 +0530

    String basic methods  in jS

---
+++ string_method.js
@@ -0,0 +1,13 @@
+let fruit = "Banana";
+let more='banana\napple';
+let arr='banana,mango,apple';
+console.log(more);
+console.log(fruit.length);
+console.log(fruit.indexOf('na'));
+console.log(fruit.slice(2,5));
+console.log(fruit.replace('ana','123'));
+console.log(fruit.toUpperCase()); 
+console.log(fruit.toLowerCase());
+console.log(fruit.charAt(2));
+console.log(fruit.split('')); //spilt by character
+console.log(arr.split(',')); //spilt by a comma
