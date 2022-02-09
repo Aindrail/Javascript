ca73b426660b2fea11b1f97363459c40ffeecffb
Aindrail <aindrailsantra@gmail.com>
2022-02-09 22:31:06 +0530

    Functions in jS

---
+++ functions.js
@@ -0,0 +1,19 @@
+function hellow(){
+    var name2 = prompt('Enter your name');
+    console.log("Welcome "+name2);
+}
+
+
+function hello(name1){
+    console.log("Hellow "+name1);
+}
+function add(num1, num2){
+    var res = num1+num2;
+    console.log(res);
+
+}
+
+hellow();
+var name1 = prompt("Enter your second name");
+hello(name1);
+ add("Please ","Go");
