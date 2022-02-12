b8ac828c80e5de890d02e7520e869238d39d18c4
Aindrail <aindrailsantra@gmail.com>
2022-02-12 23:33:40 +0530

    Challenge 3 Rock paper scissors game Js added DOM added

--- Challenges/static/js/script.js
+++ Challenges/static/js/script.js
@@ -73,6 +73,7 @@ function finalMessage([yourScore]){
 
 //this is the frond end diaplay function all the DOM happens here
 function rpsFrontEnd(humanImageChoice, botImageChoice , finalMessage){
+    //a data base is created
     var imagesDatabase = {
         'rock': document.getElementById('rock').src,
         'paper' : document.getElementById('paper').src,
@@ -82,11 +83,11 @@ function rpsFrontEnd(humanImageChoice, botImageChoice , finalMessage){
     document.getElementById('rock').remove();
     document.getElementById('paper').remove();
     document.getElementById('scissors').remove();
-
+//new div is creted each for batchoice your choice and results
     var humanDiv = document.createElement('div');
     var messageDiv = document.createElement('div');
     var botDiv = document.createElement('div');
- 
+ //innner html is changed from here
     humanDiv.innerHTML = "<img src='" + imagesDatabase[humanImageChoice.id] + "' height =250 width =250 style='box-shadow: 0px 10px 50px rgba(20, 48, 230, 0.8);' >"
    messageDiv.innerHTML="<h1 style='color: "+finalMessage['color'] +"; font-size: 60px; padding: 30px;'>"+finalMessage['message']+"</h1>";
     botDiv.innerHTML = "<img src='" + imagesDatabase[botImageChoice] + "' height =250 width =250 style='box-shadow: 0px 10px 50px rgba(243, 38, 24, 0.8);' >"
